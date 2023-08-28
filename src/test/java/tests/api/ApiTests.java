bd37766927e2b0cc35b978d415a3b42925afe72b
kotopyos <115741086+kotopyos@users.noreply.github.com>
2023-08-28 14:46:02 +0300

    separate ui and api tasks upd build.gradle

--- src/test/java/tests/api/ApiTests.java
+++ src/test/java/tests/api/ApiTests.java
@@ -5,6 +5,7 @@ import api.requests.AddToFavouriteRequestModel;
 import api.responses.AddToFavouriteResponeModel;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -12,6 +13,8 @@ import org.junit.jupiter.api.Test;
 import tests.TestBase;
 
 import static com.codeborne.selenide.Selenide.open;
+import static helpers.CustomApiListener.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,11 +30,12 @@ public class ApiTests extends TestBase {
     @Test
     @DisplayName("Add movie to favourites")
     void addMovieToFavourites(){
-        open();
         AddToFavouriteRequestModel body = new AddToFavouriteRequestModel();
         body.setId(513760);
         body.setSession("2f56cab92087821604_1708957377-721636760Le4PLzaRydPSLLUy1z82qg");
-        AddToFavouriteResponeModel res = given()
+        AddToFavouriteResponeModel res = step("Making request for adding movie to favourites", ()->
+         given()
+                .filter(withCustomTemplates())
                 .contentType(ContentType.URLENC)
                 .formParams("id", body.getId(),
                         "session", body.getSession()).relaxedHTTPSValidation()
@@ -40,10 +44,11 @@ public class ApiTests extends TestBase {
                 .then()
                 .log().body()
                 .statusCode(200)
-                .extract().as(AddToFavouriteResponeModel.class);
-
+                .extract().as(AddToFavouriteResponeModel.class)
+        );
+        step("Asserting 'result' field",()->{
         assertThat(res.getResult()).isEqualTo("ok");
-
+        });
     }
 
 }
